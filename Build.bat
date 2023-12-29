3fedf6456b600ab7ce3f8dfe020a6a46cecfd20c
Michal Ziulek <michalziulek@gmail.com>
2023-12-29 18:43:32 +0100

    Update

--- Build.bat
+++ Build.bat
@@ -1,3 +1,9 @@
+@echo off
+
+set PATH=C:\UE5
 set NAME=Sandbox
 
-UnrealEngine\Engine\Build\BatchFiles\Build.bat -Target="%NAME%Editor Win64 Development -Project=C:\UE5\%NAME%\%NAME%.uproject" -Target="ShaderCompileWorker Win64 Development -Quiet" -WaitMutex -FromMsBuild
+CALL UnrealEngine\Engine\Build\BatchFiles\Build.bat -Target="%NAME%Editor Win64 Development -Project=%PATH%\%NAME%\%NAME%.uproject" -Target="ShaderCompileWorker Win64 Development -Quiet" -WaitMutex -FromMsBuild
+IF errorlevel 1 EXIT /B !ERRORLEVEL!
+
+IF "%1"=="run" START UnrealEngine\Engine\Binaries\Win64\UnrealEditor.exe "%PATH%\%NAME%\%NAME%.uproject"
